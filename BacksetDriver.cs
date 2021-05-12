f7c1d709b5b6b02325e494a5db81e55ebf554aea
Markku Sjöblom <markku.sjoblom@gmail.com>
2021-05-12 12:09:39 +0300

    Simplified the code

---
+++ BacksetDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileSplitter
+{
+    class BacksetDriver
+    {
+        public static void ModifyFilePath(string[] args)
+        {
+            Setup._file = args[0].Split('\\');
+            for (int i = 0; i < Setup._file.Length - 1; i++)
+            {
+                Setup._path += Setup._file[i] + "\\";  //Merge it back to new global variable for later use.
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ using System.Threading;
 
 namespace TextFileSplitter
 {
+    public class Setup
+    {
+        public static string _outputFile = @"C:\Users\Markku\Desktop\TestDir\";
+        public static string[] _file;
+        public static string _path = "";
+        public static int _count = 0;
+    }
     class Program
     {
-        public static string outputFile = @"C:\Users\Markku\Desktop\TestDir\";
-        public static string[] file;
-        public static string path = "";
-        public static int COUNT = 0;
 
         static void Main(string[] args)
         {
@@ -20,18 +23,15 @@ namespace TextFileSplitter
             //Split file address to get file name
             try
             {
-                file = args[0].Split('\\');
-                for (int i = 0; i < file.Length - 1; i++)
-                {
-                    path += file[i]+"\\";  //Merge it back to new global variable for later use.
-                }
+                BacksetDriver.ModifyFilePath(args);
+                
             }
             catch (Exception)
             {
                 throw;
             }
             //Make sure that the string recieved was a file path.
-            if (!Directory.Exists(path))
+            if (!Directory.Exists(Setup._path))
             {
                 return;
             }
@@ -41,8 +41,8 @@ namespace TextFileSplitter
                 try
                 {
                     //Take 1M new lines and skip the ones already appended
-                    var firstLines = File.ReadLines(path).Skip(i * 1000000).Take(1000000).ToArray();
-                    File.AppendAllLines(outputFile + "file(" + i + 1 + ").txt", firstLines);
+                    var firstLines = File.ReadLines(Setup._path).Skip(i * 1000000).Take(1000000).ToArray();
+                    File.AppendAllLines(Setup._outputFile + "file(" + i + 1 + ").txt", firstLines);
                     Console.Write("File: " + i + "\r");
                 }
                 catch (Exception e)
